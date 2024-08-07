0c98a3055febc8aa2a95397664f59d12c080cc2a
Nikhil Singh <164791166+Nikk6174@users.noreply.github.com>
2024-08-08 00:27:32 +0530

    goal controller and route setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-console.log('hi')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const {errorhandler} = require('./middleware/errorMiddleware')
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.use('/api/goals', require('./routes/goRoutes'))
+app.use(errorhandler)
+
+app.listen(port, ()=> console.log(`sever started on port ${port}`))
